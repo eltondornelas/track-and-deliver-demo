f2e32ce60ec1695b29e8a775735621221e1932e5
Elton Dornelas <eltondornelas@gmail.com>
2026-06-06 15:43:32 -0400

    adding OrderStatusEnum and Default trait

--- src/domain/entities/order.rs
+++ src/domain/entities/order.rs
@@ -1,27 +1,30 @@
 use chrono::{DateTime, SubsecRound, Utc};
 use serde::Serialize;
 
-#[derive(Serialize)]
+#[derive(Serialize, Default)]
 pub struct Order {
     id: String,
     customer_id: String, // UUID
-    status: String,
+    status: OrderStatusEnum,
     created_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Default)]
+enum OrderStatusEnum {
+    #[default]
+    Created,
+    Approved,
+    Shipped,
+    InTransit,
+    Delivered,
+    Cancelled,
 }
 
 impl Order {
     pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Default for Order {
-    fn default() -> Self {
         Order {
-            id: "1".to_string(),
-            customer_id: "123abc321".to_string(),
-            status: "CREATED".to_string(),
             created_at: chrono::Utc::now().trunc_subsecs(3),
+            ..Default::default()
         }
     }
 }
